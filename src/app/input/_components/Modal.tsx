2e917e614360ad97159144d5af5c9635770a338b
Tomas Sullivan <tsulliva@tcd.ie>
2025-03-12 23:08:33 +0900

    start making dialog with shad ui

---
+++ src/app/input/_components/Modal.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface Props {
+  allText: string[];
+  isShowModal: boolean;
+  setIsShowModal: Function;
+}
+
+const Modal = ({ allText, isShowModal, setIsShowModal }: Props) => {
+  //modal
+  return (
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -2,16 +2,17 @@
 import { useState } from "react";
 import InputTextArea from "./_components/InputTextArea";
 import Paginator from "./_components/Paginator";
+import Modal from "./_components/Modal";
 
 function InputAndPaginate() {
   const [userText, setUserText] = useState<string>("");
   const [textArray, setTextArray] = useState<string[]>([]);
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
   const handleTextEntry = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setUserText(event.target.value);
     //split user text into an array
     //for any of these sentence endings: .?!\n。！？
-    //  let sentenceEndings = [".", "?", "!", "\n", "。", "！", "？"];
     setTextArray(
       event.target.value.split(/[\.\?!\n。！？]/).filter((x) => x !== ""),
     );
@@ -24,12 +25,16 @@ function InputAndPaginate() {
   const paginatorProps = {
     allText: textArray,
   };
+  const modalProps = {
+    allText: textArray,
+  };
   return (
-    <div className="width-screen h-screen bg-gray-800 text-gray-100">
+    <div className="h-screen w-screen bg-gray-800 text-gray-100">
       {/* ... other content ... */}
       <InputTextArea {...textAreaProps} />
       {/* ... more content ... */}
       <Paginator {...paginatorProps} />
+      <Modal {...modalProps} />
     </div>
   );
 }
