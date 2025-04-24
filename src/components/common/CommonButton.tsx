8ab939f4171978761fc0a7416de2e11e17bbed36
Tomas Sullivan <tsulliva@tcd.ie>
2025-04-24 21:49:23 +0900

    split custom props from inbuild button props

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -7,14 +7,19 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   isTempDisabled?: boolean;
 }
 
-const CommonButton: React.FC<Props> = ({ label, ...props }) => {
+const CommonButton: React.FC<Props> = ({
+  label,
+  additionalclasses,
+  isTempDisabled,
+  ...props
+}) => {
   const baseStyles =
     "focus:outline-none mx-2 my-2 rounded-xl border-2 border-blue-100/20 bg-blue-500/50 px-4 py-2 shadow-md hover:bg-blue-400 hover:text-black";
 
-  let styleString: string = twMerge(baseStyles, props.additionalclasses);
+  let styleString: string = twMerge(baseStyles, additionalclasses);
 
   //if isDisabled is true, add disabled styles
-  if (props.isTempDisabled) {
+  if (isTempDisabled) {
     styleString = twMerge(
       styleString,
       " cursor-not-allowed opacity-50 hover:bg-blue-500/50 hover:text-black",
