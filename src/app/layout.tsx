ef7368d240b8fe27fefd73da1a67d8cd9d05aa19
Tomas Sullivan <tsulliva@tcd.ie>
2025-12-26 11:41:09 +0900

    move page styling to layout.tsx from the custom page container component. Need to remove the page container later.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,22 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { SettingsProvider } from "./SettingsProvider";
 
 export const metadata: Metadata = {
-  title: "Japanese Ereader",
-  description: "A tool to help students of Japanese read and understand difficult texts.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "Japanese Ereader",
+    description:
+        "A tool to help students of Japanese read and understand difficult texts.",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <SettingsProvider>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </SettingsProvider>
-      </body >
-    </html >
-  );
+    return (
+        <html lang="en" className={`${GeistSans.variable}`}>
+            <body className="h-screen w-full overflow-x-hidden bg-gradient-to-b from-slate-900 to-slate-800 text-gray-300">
+                <SettingsProvider>
+                    <TRPCReactProvider>{children}</TRPCReactProvider>
+                </SettingsProvider>
+            </body>
+        </html>
+    );
 }
--- src/components/common/PageContainer.tsx
+++ src/components/common/PageContainer.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
 export default function PageContainer({ ...props }) {
-    return (
-        <div className="h-screen w-full overflow-x-hidden bg-gradient-to-b from-slate-900 to-slate-800 text-gray-300">
-            {props.children}
-        </div>
-    );
+    return <div>{props.children}</div>;
 }
